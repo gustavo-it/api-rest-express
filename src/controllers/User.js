636c43d82157218ea45e2bc93595a38a4f50aa62
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-05-26 14:25:59 -0300

    CRUD de usuários

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -4,12 +4,80 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch(error) {
       console.log(error)
-      res.status(400).json({
+      return res.status(400).json({
         errors: error.errors.map(error => error.message)
       });
+    }
+  }
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll(); // buscando todos os usuarios no db
+      return res.json(users);
+    }catch(error) {
+      return res.json(null);
+    }
+  }
+
+  // Show
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id); // buscando usuário por pk
+      return res.json(user);
+    }catch(error) {
+      return res.json(null);
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if(!id) {
+        return res.status(400).json({
+          errors: ['Id não enviado.'],
+        });
+      }
+      const user = await User.findByPk(id);
+
+      if(!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    }catch(error) {
+      return res.json(null);
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if(!id) {
+        return res.status(400).json({
+          errors: ['Id não enviado.'],
+        });
+      }
+      const user = await User.findByPk(id);
+
+      if(!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    }catch(error) {
+      return res.json(null);
     }
   }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,10 @@ const router = new Router();
 import userController from '../controllers/User';
 
 router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
 
